be9950a2cef88e5bed00979b8fea6fdbc4bb5e84
Evan Feng <evfe2789@colorado.edu>
2025-03-31 01:50:46 -0600

    Added personal panoramas and changed it to a database system for pano info

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,4 +1,27 @@
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(60) NOT NULL
+    password VARCHAR(60) NOT NULL,
+    geoguessr_high_score INT DEFAULT 0,  -- Stores the best score in Geoguessr
 );
+
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL, --Location Name
+    image_file VARCHAR(255) NOT NULL,  -- image file name
+    latitude DECIMAL(9,6) NOT NULL,  -- Latitude 
+    longitude DECIMAL(9,6) NOT NULL,  -- Longitude
+);
+
+
+CREATE TABLE geoGuessrScoresTable (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    location_id INT NOT NULL,
+    score INT NOT NULL,  -- Points awarded based on distance
+    distance DECIMAL(6,2) NOT NULL,  -- How far they were (in km)
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (location_id) REFERENCES locations(id) ON DELETE CASCADE
+);
+
---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO locations (name, image_file, latitude, longitude) VALUES
+('Folsom Statue', 'ArtBuilding.jpg', 40.01073, 105.26375),
+('Business Field', 'BusinessField.jpg', 40.006120, 105.262480),
+('Farrand Field', 'FarrandField.jpg', 40.00641, 105.26665)
+('Art Building', 'ArtBuilding.jpg', 40.00712, 105.27027)
+('Planetarium', 'Planetarium.jpg', 40.00354, 105.26397);
