a6fdfcdab3a1f38f6728e0c4d2ea13f542775bed
Anonas3675 <104173903+Anonas3675@users.noreply.github.com>
2025-04-03 14:26:11 -0600

    Removed unused files

--- ProjectSourceCode/public/resources/js/geoguess.js
+++
@@ -1,8 +0,0 @@
-import { Viewer } from '@photo-sphere-viewer/core';
-
-const baseUrl = 'https://photo-sphere-viewer-data.netlify.app/assets/';
-
-new Viewer({
-    container: 'viewer',
-    panorama: 'resources/hdri_sky_860.jpg',
-});
