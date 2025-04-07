7f61f036dc2af233c94197c609ec71ff3848ab02
Evan Feng <evfe2789@colorado.edu>
2025-04-07 02:11:04 -0600

    Nearly finished features of geoguessr. Correct grading is complete and system to confirm guess implemented. lastly they can choose to play again and added pin as UI.

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -45,11 +45,11 @@
 -- Geo_Guessr_Location
 
 INSERT INTO locations (name, image_file, latitude, longitude) VALUES 
-        ('Folsom Statue', 'ArtBuilding.jpg', 40.01073, 105.26375),
-        ('Business Field', 'BusinessField.jpg', 40.006120, 105.262480),
-        ('Farrand Field', 'FarrandField.jpg', 40.00641, 105.26665),
-        ('Art Building', 'ArtBuilding.jpg', 40.00712, 105.27027),
-        ('Planetarium', 'Planetarium.jpg', 40.00354, 105.26397);
+        ('Folsom Statue', 'ArtBuilding.jpg', 40.008711, -105.263675),
+        ('Business Field', 'BusinessField.jpg', 40.007184, -105.263365),
+        ('Farrand Field', 'FarrandField.jpg', 40.007344, -105.264509),
+        ('Art Building', 'ArtBuilding.jpg', 40.007564, -105.265641),
+        ('Planetarium', 'Planetarium.jpg', 40.006408, -105.263686);
 
 
 
