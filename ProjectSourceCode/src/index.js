8717c933e751f1da7389c02b7df40420365bbbb0
Twinularity <dylan.t.sandusky@gmail.com>
2025-04-09 23:07:54 -0600

    Saving Wordle Guesses
    Minor changes I made in an attempt to save wordle Guesses, Unfinished.
    
    Wordle Guesses Saved to session
    
    Wordle guesses are now both saved to session and reloaded when you reload the wordle page. Made modifications to wordle.js only.

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -212,7 +212,6 @@ const auth = (req, res, next) => {
 app.use(auth)
 
 
-
 app.get('/home', auth, async (req, res) => {
   res.render('pages/home', {
       games: [
@@ -307,11 +306,6 @@ app.get('/trivia', (req, res) => {
   res.render('pages/trivia');
 })
 
-//Wordel APIs
-app.get('/wordle', (req, res) => {
-  res.render('pages/wordle');
-})
-
 app.get('/question', async (req, res) => {
   const difficulty = req.query.difficulty;
   try {
@@ -323,7 +317,35 @@ app.get('/question', async (req, res) => {
   }
 });
 
-//API's for Corssword
+//Wordle APIs
+
+//This renders the wordle page 
+app.get('/wordle', (req, res) => {
+  res.render('pages/wordle');
+})
+
+//Whenever a guess gets made, make a call to this. This should store the guess in the server
+app.post('/saveguess', (req, res) => {
+  const guess = req.body.guess;
+
+  // Initialize guesses array if needed
+  if (!req.session.guesses) {
+    req.session.guesses = [];
+  }
+
+  req.session.guesses.push(guess);
+  req.session.save();
+  //After a request is made to save a guess, it returns all of the saved guesses
+  res.json({ message: "Guess saved", guesses: req.session.guesses });
+});
+
+//Still need a wordle API that will handle loading perviously made guesses
+app.get('/getguess', (req, res) => {
+  const previousGuesses = req.session.guesses || [];
+  res.json(previousGuesses);
+});
+
+//API's for Crossword
 
 // Get all puzzles
 app.get('/puzzles', async (req, res) => {
